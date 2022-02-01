5a1165e32d9ec023b2e255e8ef4b87386186c82f
Lapinas <mrlapinas99@gmail.com>
2022-02-01 10:35:27 +0000

    Added app.js info

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,12 +4,13 @@ const argv = yargs(hideBin(process.argv)).argv;
 
 let title = argv.title;
 let actor = argv.actor;
+let year = argv.year;
 
 
 const app = () => {
 
     if (argv.add) {
-        console.log(`Movie ${title} with actor ${actor} added.`);
+        console.log(`Movie ${title} with actor ${actor} added. Was released in: ${year}.`);
     }
 
 };
